3630c920af0aeb06fe2a1b90685f3bb062bf796b
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-08 15:03:46 +0100

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/to-do-list-app-nextjs/' : '',
+  basePath: isProd ? '/to-do-list-app-nextjs' : '',
+  output: 'export',
+};
 
 export default nextConfig;
