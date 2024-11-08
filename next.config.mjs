35494cd01c7a9d96a4eea3ab2d58221cd9323275
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-08 16:00:27 +0100

    changes in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, 
+  },
+  assetPrefix: isProd ? '/katelijacobsen/' : '',
+  basePath: isProd ? '/to-do-list-app-nextjs' : '',
+  output: 'export'
+};
 
 export default nextConfig;
