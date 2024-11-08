6d52ef03d7a63cb1a3566ba57d949fcdd7175280
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-08 13:50:25 +0100

    my poor typo...

--- src/app/components/ListItem.jsx
+++ src/app/components/ListItem.jsx
@@ -2,7 +2,7 @@ const ListItem = ({task, deleteTask, toggleTask}) => {
     return ( <li>
         <p style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</p>
         <button onClick={() => toggleTask(task.id)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">
-            {task.complete ? "Undo" : "Complete"}</button>
+            {task.completed ? 'Undo' : 'Complete'}</button>
         <button onClick={() => deleteTask(task.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">Delete</button>
     </li> );
 }
